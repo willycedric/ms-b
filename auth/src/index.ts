6a11db3c45a8fd662acf60d9ab5586a6a569036a
Willy DONGMO <w.dongmo@gmail.com>
2021-11-27 16:30:56 +0100

    .

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
+ 
 import { currentuserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { singoutRouter } from './routes/signout';
@@ -19,6 +21,16 @@ app.all('*', async ()=>{
 })
 app.use(errorHandler);
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000 !!!');
-}); 
+const start = async ()=>{
+    try{
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log('Connecting to MongoDb');
+    }catch(err){
+        console.error(err);
+    }
+    app.listen(3000, ()=>{
+        console.log('listening on port 3000 !!!');
+    });  
+};
+start();
+
